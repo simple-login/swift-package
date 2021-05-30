180b633a9c1d33dbc4d44b604a42e341dda5df21
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2021-05-30 11:44:23 +0200

    Make model public

---
+++ Sources/SimpleLoginPackage/Model/Alias.swift
@@ -0,0 +1,16 @@
+//
+//  Alias.swift
+//  Swift Package
+//
+//  Created by Thanh-Nhon Nguyen on 30/05/2021.
+//
+
+public struct Person {
+    public let name: String
+    public let age: Int
+
+    public init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
