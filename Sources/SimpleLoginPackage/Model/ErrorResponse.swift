29683a3b2445c5b04cf38767aee8a3059b601d45
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2021-08-29 20:42:21 +0200

    Add missing ErrorResponse.swift

---
+++ Sources/SimpleLoginPackage/Model/ErrorResponse.swift
@@ -0,0 +1,25 @@
+//
+//  ErrorResponse.swift
+//  Swift PackageTests
+//
+//  Created by Thanh-Nhon Nguyen on 05/06/2021.
+//
+
+import Foundation
+
+public struct ErrorResponse: Decodable {
+    public let message: String
+    public let code: String?
+
+    // swiftlint:disable:next type_name
+    private enum Key: String, CodingKey {
+        case message = "error"
+        case code = "error_code"
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Key.self)
+        self.message = try container.decode(String.self, forKey: .message)
+        self.code = try container.decodeIfPresent(String.self, forKey: .code)
+    }
+}
