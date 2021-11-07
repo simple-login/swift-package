dae78c5f84e064cd11c5ff7bd9c4c51ae222854b
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2021-11-07 15:17:42 +0100

    Convenience Alias extensions

--- Sources/SimpleLoginPackage/Model/Alias.swift
+++ Sources/SimpleLoginPackage/Model/Alias.swift
@@ -61,6 +61,34 @@ extension Alias: Decodable {
     }
 }
 
+public extension Alias {
+    var mailboxesString: String {
+        mailboxes.map { $0.email }.joined(separator: ", ")
+    }
+
+    var creationDate: Date {
+        Date(timeIntervalSince1970: creationTimestamp)
+    }
+
+    var creationDateString: String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        formatter.doesRelativeDateFormatting = true
+        return formatter.string(from: creationDate)
+    }
+
+    var relativeCreationDateString: String {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.dateTimeStyle = .named
+        return formatter.string(for: creationDate) ?? ""
+    }
+
+    var noActivities: Bool {
+        forwardCount + replyCount + blockCount == 0
+    }
+}
+
 public struct AliasArray: Decodable {
     public let aliases: [Alias]
 }
