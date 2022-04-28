f9a691ab54d208b6b27af360e4eeaa139a924de0
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2022-04-28 19:06:00 +0200

    Add enabled AliasFilterOption

--- Sources/SimpleLoginPackage/Model/AliasFilterOption.swift
+++ Sources/SimpleLoginPackage/Model/AliasFilterOption.swift
@@ -9,11 +9,13 @@ import Foundation
 
 public enum AliasFilterOption {
     case disabled
+    case enabled
     case pinned
 
     public var queryItem: URLQueryItem {
         switch self {
         case .disabled: return .init(name: "disabled", value: nil)
+        case .enabled: return .init(name: "enabled", value: nil)
         case .pinned: return .init(name: "pinned", value: nil)
         }
     }
