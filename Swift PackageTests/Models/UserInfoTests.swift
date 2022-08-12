26e196dff2d944093a6253c33e0091b39b56999f
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-08-12 10:20:21 +0200

    UserInfo - Add maxAliasFreePlan, isConnectedWithProton & unit tests

---
+++ Swift PackageTests/Models/UserInfoTests.swift
@@ -0,0 +1,31 @@
+//
+//  UserInfoTests.swift
+//  Swift PackageTests
+//
+//  Created by Nhon Proton on 12/08/2022.
+//
+
+@testable import Swift_Package
+import XCTest
+
+final class UserInfoTests: XCTestCase {
+    func testDecodable() throws {
+        let userInfo1 = try JSONDecoder().decode(UserInfo.self, from: MockedData.userInfo1)
+        XCTAssertEqual(userInfo1.name, "John Doe")
+        XCTAssertFalse(userInfo1.isPremium)
+        XCTAssertEqual(userInfo1.email, "john@doe.com")
+        XCTAssertTrue(userInfo1.inTrial)
+        XCTAssertNil(userInfo1.profilePictureUrl)
+        XCTAssertEqual(userInfo1.maxAliasFreePlan, 12)
+        XCTAssertTrue(userInfo1.isConnectedWithProton)
+
+        let userInfo2 = try JSONDecoder().decode(UserInfo.self, from: MockedData.userInfo2)
+        XCTAssertEqual(userInfo2.name, "Jane Doe")
+        XCTAssertTrue(userInfo2.isPremium)
+        XCTAssertEqual(userInfo2.email, "jane@doe.com")
+        XCTAssertFalse(userInfo2.inTrial)
+        XCTAssertEqual(userInfo2.profilePictureUrl, "https://profile.png")
+        XCTAssertEqual(userInfo2.maxAliasFreePlan, 7)
+        XCTAssertFalse(userInfo2.isConnectedWithProton)
+    }
+}
