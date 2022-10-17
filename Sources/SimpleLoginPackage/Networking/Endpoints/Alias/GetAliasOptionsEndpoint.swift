d4c40605f673efba986ca6f0dd7a39725b638448
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-10-17 17:30:34 +0200

    Implement GET /api/v5/alias/options

---
+++ Sources/SimpleLoginPackage/Networking/Endpoints/Alias/GetAliasOptionsEndpoint.swift
@@ -0,0 +1,26 @@
+//
+//  GetAliasOptionsEndpoint.swift
+//  Swift Package
+//
+//  Created by Nhon Proton on 17/10/2022.
+//
+
+import Foundation
+
+// GET /api/v5/alias/options
+// https://github.com/simple-login/app/blob/master/docs/api.md#get-apiv5aliasoptions
+
+public struct GetAliasOptionsEndpoint: EndpointV2 {
+    public typealias Body = EmptyBody
+    public typealias Response = AliasOptions
+
+    public var path: String
+    public var method: HTTPMethod
+    public var apiKey: String?
+
+    public init(apiKey: String) {
+        self.path = "/api/v5/alias/options"
+        self.method = .get
+        self.apiKey = apiKey
+    }
+}
