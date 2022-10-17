c6ee659bb754e57a4616511ab68b716dc492c3fc
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-10-17 17:54:06 +0200

    Implement GET /api/v2/mailboxes & POST /api/v3/alias/custom/new

--- Sources/SimpleLoginPackage/Model/Mailbox.swift
+++ Sources/SimpleLoginPackage/Model/Mailbox.swift
@@ -7,13 +7,22 @@
 
 import Foundation
 
-public struct Mailbox {
+public struct Mailbox: Decodable {
     public let id: Int
     public let email: String
     public let `default`: Bool
     public let creationTimestamp: TimeInterval
     public let aliasCount: Int
     public let verified: Bool
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case email = "email"
+        case `default` = "default"
+        case creationTimestamp = "creation_timestamp"
+        case aliasCount = "nb_alias"
+        case verified = "verified"
+    }
 
     public init(id: Int,
                 email: String,
@@ -27,27 +36,6 @@ public struct Mailbox {
         self.creationTimestamp = creationTimestamp
         self.aliasCount = aliasCount
         self.verified = verified
-    }
-}
-
-extension Mailbox: Decodable {
-    private enum Key: String, CodingKey {
-        case id = "id"
-        case email = "email"
-        case `default` = "default"
-        case creationTimestamp = "creation_timestamp"
-        case aliasCount = "nb_alias"
-        case verified = "verified"
-    }
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: Key.self)
-        self.id = try container.decode(Int.self, forKey: .id)
-        self.email = try container.decode(String.self, forKey: .email)
-        self.default = try container.decode(Bool.self, forKey: .default)
-        self.creationTimestamp = try container.decode(TimeInterval.self, forKey: .creationTimestamp)
-        self.aliasCount = try container.decode(Int.self, forKey: .aliasCount)
-        self.verified = try container.decode(Bool.self, forKey: .verified)
     }
 }
 
---
+++ Sources/SimpleLoginPackage/Networking/Endpoints/Mailbox/GetMailboxesEndpoint.swift
@@ -0,0 +1,26 @@
+//
+//  GetMailboxesEndpoint.swift
+//  Swift Package
+//
+//  Created by Nhon Proton on 17/10/2022.
+//
+
+import Foundation
+
+// GET /api/v2/mailboxes
+// https://github.com/simple-login/app/blob/master/docs/api.md#get-apiv2mailboxes
+
+public struct GetMailboxesEndpoint: EndpointV2 {
+    public typealias Body = EmptyBody
+    public typealias Response = MailboxArray
+
+    public var path: String
+    public var method: HTTPMethod
+    public var apiKey: String?
+
+    public init(apiKey: String) {
+        self.path = "/api/v2/mailboxes"
+        self.method = .get
+        self.apiKey = apiKey
+    }
+}
