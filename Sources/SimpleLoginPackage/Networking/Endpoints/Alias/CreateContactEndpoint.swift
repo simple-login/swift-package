1e68399a6e0f02014bcea49c12036d761aa842ce
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-10-18 09:07:48 +0200

    Implement POST /api/aliases/:alias_id/contacts

---
+++ Sources/SimpleLoginPackage/Networking/Endpoints/Alias/CreateContactEndpoint.swift
@@ -0,0 +1,35 @@
+//
+//  CreateContactEndpoint.swift
+//  Swift Package
+//
+//  Created by Nhon Proton on 18/10/2022.
+//
+
+import Foundation
+
+// POST /api/aliases/:alias_id/contacts
+// https://github.com/simple-login/app/blob/master/docs/api.md#post-apialiasesalias_idcontacts
+public struct CreateContactEndpoint: EndpointV2 {
+    public typealias Body = CreateContactRequest
+    public typealias Response = Contact
+
+    public var path: String
+    public var method: HTTPMethod
+    public var apiKey: String?
+    public var body: CreateContactRequest?
+
+    public init(apiKey: String, aliasID: AliasID, email: String) {
+        self.path = "/api/aliases/\(aliasID)/contacts"
+        self.method = .post
+        self.apiKey = apiKey
+        self.body = .init(email: email)
+    }
+}
+
+public struct CreateContactRequest: Encodable {
+    let email: String
+
+    private enum CodingKeys: String, CodingKey {
+        case email = "contact"
+    }
+}
