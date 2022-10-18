ca62d02701cc71ed77cec8b5a15ffac752fbfe00
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-10-18 12:44:27 +0200

    Implement DELETE /api/setting/unlink_proton_account

---
+++ Sources/SimpleLoginPackage/Networking/Endpoints/Settings/UnlinkProtonAccountEndpoint.swift
@@ -0,0 +1,24 @@
+//
+//  UnlinkProtonAccountEndpoint.swift
+//  Swift Package
+//
+//  Created by Nhon Proton on 18/10/2022.
+//
+
+import Foundation
+
+// DELETE /api/setting/unlink_proton_account
+public struct UnlinkProtonAccountEndpoint: EndpointV2 {
+    public typealias Body = EmptyBody
+    public typealias Response = OkResponse
+
+    public var path: String
+    public var method: HTTPMethod
+    public var apiKey: String?
+
+    public init(apiKey: String) {
+        self.path = "/api/setting/unlink_proton_account"
+        self.method = .delete
+        self.apiKey = apiKey
+    }
+}
