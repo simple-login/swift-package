5e26aed734ff4acf849b19dd51064d48b6efcbbc
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2022-10-18 08:29:32 +0200

    Implement GET /api/aliases/:alias_id/activities

--- Sources/SimpleLoginPackage/Model/AliasActivity.swift
+++ Sources/SimpleLoginPackage/Model/AliasActivity.swift
@@ -7,33 +7,21 @@
 
 import Foundation
 
-public struct AliasActivity {
+public struct AliasActivity: Decodable {
     public let action: ActivityAction
     public let timestamp: TimeInterval
     public let from: String
     public let to: String
     public let reverseAlias: String // "\"marketing at example.com\" <reply@a.b>"
     public let reverseAliasAddress: String // "reply@a.b"
-}
 
-extension AliasActivity: Decodable {
-    private enum Key: String, CodingKey {
+    private enum CodingKeys: String, CodingKey {
         case action = "action"
         case timestamp = "timestamp"
         case from = "from"
         case to = "to"
         case reverseAlias = "reverse_alias"
         case reverseAliasAddress = "reverse_alias_address"
-    }
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: Key.self)
-        self.action = try container.decode(ActivityAction.self, forKey: .action)
-        self.timestamp = try container.decode(TimeInterval.self, forKey: .timestamp)
-        self.from = try container.decode(String.self, forKey: .from)
-        self.to = try container.decode(String.self, forKey: .to)
-        self.reverseAlias = try container.decode(String.self, forKey: .reverseAlias)
-        self.reverseAliasAddress = try container.decode(String.self, forKey: .reverseAliasAddress)
     }
 }
 
