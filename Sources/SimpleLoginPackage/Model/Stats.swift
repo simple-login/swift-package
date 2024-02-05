d8ff684c2f4fa1060700fa087876cbf0a5f5e40a
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2024-02-05 18:17:24 +0700

    feat: add get stats endpoint

---
+++ Sources/SimpleLoginPackage/Model/Stats.swift
@@ -0,0 +1,36 @@
+//
+// Stats.swift
+// SimpleLogin - Created on 05/02/2024.
+// Copyright (c) 2024 Proton Technologies AG
+//
+// This file is part of SimpleLogin.
+//
+// SimpleLogin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SimpleLogin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with SimpleLogin. If not, see https://www.gnu.org/licenses/.
+//
+
+import Foundation
+
+public struct Stats: Decodable {
+    public let aliasCount: Int
+    public let blockCount: Int
+    public let forwardCount: Int
+    public let replyCount: Int
+
+    private enum CodingKeys: String, CodingKey {
+        case aliasCount = "nb_alias"
+        case blockCount = "nb_block"
+        case forwardCount = "nb_forward"
+        case replyCount = "nb_reply"
+    }
+}
